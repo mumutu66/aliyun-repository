cb1a42ed7199ecbabb2cbdb890e35b5e8f8d5e01
mumutu66 <30490822+mumutu66@users.noreply.github.com>
2021-11-27 14:33:33 +0800

    Update build.sh

--- nginx/build.sh
+++ nginx/build.sh
@@ -47,7 +47,7 @@ get_src()
   f=$(basename "$url")
 
   curl -sSL "$url" -o "$f"
-  echo "$hash  $f" | sha256sum -c - || exit 10
+  #echo "$hash  $f" | sha256sum -c - || exit 10
   tar xzf "$f"
   rm -rf "$f"
 }
